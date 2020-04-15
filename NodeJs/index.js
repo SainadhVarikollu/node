c55aa6858ddf4bdf06624886f520f9d45e25a26a
SainadhVarikollu <sainadhvarikollu97@gmail.com>
2020-04-15 16:43:51 +0530

    first commit

---
+++ NodeJs/index.js
@@ -0,0 +1,20 @@
+var rect=require('./rectangle')
+function solveRect(l,b){
+    console.log(`Solving for Rectangle's l=${l} and b=${b}`)
+    
+    
+    rect(l,b,(err,rectangle)=>{
+        if(err){
+            console.log(`Error: ${err.message}`);
+        }
+        else{
+            console.log(`The area of Rectangle of dimensions l=${l} and b=${b} is ${rectangle.area()}`);
+            console.log(`The Perimeter of Rectangle of dimensions l=${l} and b=${b} is ${rectangle.perimeter()}`);
+        }
+    });
+    console.log("This statement after the call to rect()");
+}
+solveRect(2,4);
+solveRect(3,5);
+solveRect(0,5);
+solveRect(-3,5);
